9956ad622cdf73d502e5fffdfc3b452938a24879
LvXiaoBu <49021590+uinio@users.noreply.github.com>
2022-09-26 19:06:46 +0800

    json validation

--- packages/index.ts
+++ packages/index.ts
@@ -16,7 +16,7 @@ export interface PiniaPersistOptions {
 }
 
 function isJSON(value: string) {
-  return /^\{([^/]{0,})\}$/.test(value) || /^\[([^/]{0,})\]$/.test(value);
+  return /^\{([^]{0,})\}$/.test(value) || /^\[([^]{0,})\]$/.test(value);
 }
 
 function getState<T = any>(value?: string): T | undefined {
